7ecd2b96bb49efd1b7940ee35cb2d053c0dc4f5f
NinjasWeb <hola@ninjasweb.com>
2023-06-20 14:29:57 -0500

    Adding styles and cleaning

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+"use client"
 import styles from './page.module.css'
 import Image from 'next/image'
 import Logo from '../assets/logo_white.png'
 import Isologo from '@/assets/ninjasweb_white.svg'
 import Link from 'next/link'
 import { Links } from '@/config/data'
+import { useState } from 'react'
 
 export default function Home() {
+  const [isHovered, setIsHovered] = useState(-1)
   return (
     <>
       <main className={styles.main}>
@@ -18,14 +21,18 @@ export default function Home() {
             {Links.map((link, index)=>{
               return (
                 <Link href={link.url} key={index}>
-                  <div className={styles.link}>
-                    <Image src={link.icon} alt={link.title} width={50} height={50} />
+                  <div 
+                    onMouseEnter={() => setIsHovered(index)}
+                    onMouseLeave={() => setIsHovered(-1)} 
+                    className={styles.link}>
+                    <Image style={{ filter: `brightness(0) invert(${index === isHovered ? 0 : 1})` }} className={styles.icon} src={link.icon} alt={link.title} width={40} height={40} />
                     <p className={styles.title}>{link.title}</p>
                   </div>
                 </Link>
               )
             })}
           </div>
+          <p className={styles.promo}>¿Quieres crear tu propia página?</p>
         </div>
         <div className={styles.footer}>
           <div>
