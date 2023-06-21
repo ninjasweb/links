6cd3633d61702f89af09da4fca70c83c89273fb1
NinjasWeb <hola@ninjasweb.com>
2023-06-20 21:08:11 -0500

    Links Letterspacing and subtitle

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -2,12 +2,16 @@ import styles from './header.module.css'
 import Image from "next/image"
 import Logo from '@/assets/logo_white.png'
 import Isologo from '@/assets/ninjasweb_white.svg'
+import Link from 'next/link'
 
 const Header = () => {
   return ( 
     <div className={styles.header}>
-      <Image priority src={Logo.src} width={90} height={90} alt="Ninjasweb"/>
+      <Link href="/">
+        <Image priority src={Logo.src} width={90} height={90} alt="Ninjasweb"/>
+      </Link>
       <Image priority src={Isologo} width={200} height={100} alt='Ninjasweb'/>
+      <p>Convierte tus visitantes en clientes con nuestras soluciones de software.</p>
     </div>
    )
 }
